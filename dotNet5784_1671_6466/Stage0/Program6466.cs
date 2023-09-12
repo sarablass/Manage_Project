ecdebc8f42cd6cbae930ed42c17292ba9d3c4b78
sarablass <sarablass6236@gmail.com>
2023-09-12 18:20:18 +0300

    local code +Welcome function

---
+++ dotNet5784_1671_6466/Stage0/Program6466.cs
@@ -0,0 +1,21 @@
+﻿namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome6466();
+            Welcome1671();
+
+            Console.ReadKey();
+        }
+
+        static partial void Welcome1671();
+       private static  void Welcome6466() 
+        {
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+    }
+}
