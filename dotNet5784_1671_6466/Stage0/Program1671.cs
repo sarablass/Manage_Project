4682f93f683f67028d06689920594cc201118949
brachikim <brachikim@gmail.com>
2023-10-17 19:32:07 +0300

    local code +Welcome function

--- dotNet5784_1671_6466/Stage0/Program1671.cs
+++ dotNet5784_1671_6466/Stage0/Program1671.cs
@@ -10,7 +10,7 @@ namespace Stage0
     {
         static partial void Welcome1671()
         {
-            Console.WriteLine("I am also here!");
+            Console.WriteLine("I am also here");
         }
     }
 }
