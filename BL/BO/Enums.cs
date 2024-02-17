3767f5e65fca46dbd242a721347e297f71e7a7e7
sarablass <sarablass6236@gmail.com>
2024-02-17 21:44:02 +0200

    Final commit - The end of the project -congratulation!!!

--- BL/BO/Enums.cs
+++ BL/BO/Enums.cs
@@ -1,3 +1,3 @@
 ﻿namespace BO;
-public enum EngineerExperience {None, Beginner, AdvancedBeginner, Intermediate, Advanced, Expert }
-public enum Status {None ,Unscheduled, Scheduled, OnTrack, InJeopardy, Finished }
+public enum EngineerExperience {None, Beginner, AdvancedBeginner, Intermediate, Advanced, Expert } //engineer experience enum
+public enum Status {None ,Unscheduled, Scheduled, OnTrack, InJeopardy, Finished } //status enum
